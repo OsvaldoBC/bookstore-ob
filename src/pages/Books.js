763c819426e6fd59225565a85e2689b7a813944b
Osvaldo Barrios Cortina <osvaldo.barrios@gmail.com>
2023-01-20 11:37:17 -0500

    add BookList Component

--- src/pages/Books.js
+++ src/pages/Books.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import BookForm from '../components/BookForm';
+import BookList from '../components/BookList';
 
 const Books = () => (
   <div className="container" style={{ padding: '1em 3em' }}>
     <div className="book-list">
+      <BookList title="book1" author="osvaldo" />
       <BookForm /> 
   </div>
   </div>
