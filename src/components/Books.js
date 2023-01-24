246392100e984370261a8efb12c0a594ac6dfb3c
Osvaldo Barrios Cortina <osvaldo.barrios@gmail.com>
2023-01-24 18:53:28 -0500

    use redux in react Components

--- src/components/Books.js
+++ src/components/Books.js
@@ -1,13 +1,17 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import BookForm from './BookForm';
 import BookList from './BookList';
 
-const Books = () => (
-  <div className="container" style={{ padding: '1em 3em' }}>
-    <div className="book-list">
-      <BookList title="book1" author="osvaldo" />
+
+const Books = () => {
+  const books = useSelector((state) => state.books);
+  return (
+    <div>
+      <h1>Books</h1>
+      <BookList title={books[0].title} author={books[0].author} />
       <BookForm />
     </div>
-  </div>
-);
+  );
+};
 export default Books;
---
+++ src/redux/books/initialBookList.js
@@ -0,0 +1,18 @@
+const bookList = [
+    {
+      id: 1,
+      title: 'El Coronel no tiene quien lo escriba',
+      author: 'Gabriel Garcia Marquez',
+    }, {
+      id: 2,
+      title: 'El origen del Universo',
+      author: 'Steven Hawking',
+    }, {
+      id: 3,
+      title: 'El Alqimista',
+      author: 'Paulo Coelho',
+    },
+  ];
+  
+  export default bookList;
+  
