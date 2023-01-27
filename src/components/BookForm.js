385c99971e798b310c438d81e948470e814b9407
Osvaldo Barrios Cortina <osvaldo.barrios@gmail.com>
2023-01-27 14:48:24 -0500

    add styling to Bookform

--- src/components/BookForm.js
+++ src/components/BookForm.js
@@ -3,6 +3,7 @@ import { useDispatch } from 'react-redux';
 import { v4 as uuidv4 } from 'uuid';
 import { addBook } from '../redux/books/books';
 import asyncAddBook from '../redux/books/BooksAsync';
+import './BookForm.css';
 
 const initialState = {
   id: '',
@@ -24,18 +25,28 @@ const BookForm = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    dispatch(asyncAddBook(newBook));
-    dispatch(addBook(newBook));
-    e.target.reset();
+    //dispatch(asyncAddBook(newBook));
+    //dispatch(addBook(newBook));
+    const [title, author, category] = e.target;
+    dispatch(addBook({
+      title: title.value,
+      author: author.value,
+      category: category.value,
+      item_id: uuidv4(),
+    }));
+
+  title.value = '';
+    author.value = '';
+    category.value = '';
   };
 
   return (
-    <div>
-      <h2>Add a new book</h2>
-      <form onSubmit={handleSubmit}>
-        <input type="text" name="title" onChange={handleChange} placeholder="Book title" />
-        <input type="text" name="author" onChange={handleChange} placeholder="Book author" />
-        <input type="text" name="category" onChange={handleChange} placeholder="Book category" />
+    <div className="add-book-container">
+      <h2>ADD NEW BOOK</h2>
+      <form onSubmit={handleSubmit} className="add-form">
+        <input type="text" className="title" name="title" placeholder="Book Title" required />
+        <input type="text" className="author" name="author" placeholder="Author" required />
+        <input type="text" className="category" name="category" placeholder="Category" required />
         <button type="submit">ADD BOOK</button>
       </form>
     </div>
